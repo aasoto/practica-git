e4e848b7664a750501d14ba599ceba6a572b2fb0
aasoto <andresalfredosotosuarez@gmail.com>
2023-03-20 22:00:35 -0500

    feat: content page made

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
-
+  import ContentApp from "./components/ContentApp.vue";
 </script>
 
 <template>
-<h1 class="text-3xl font-bold underline">APP PRACTICA DE GITHUB</h1>
+<ContentApp/>
 </template>
 
---
+++ src/components/ContentApp.vue
@@ -0,0 +1,43 @@
+<script setup>
+</script>
+
+<template>
+  <section class="p-5 flex flex-col gap-5">
+    <div class="bg-white shadow rounded-lg border-t-4 border-blue-800 w-full">
+      <div class="px-10 py-4 border-b border-black">
+        <h2 class="text-xl font-bold">New York City</h2>
+      </div>
+      <div class="px-10 py-4">
+        <div class="grid grid-cols-3 gap-4">
+          <div class="col-span-1">
+            <svg class="w-52 h-52 text-blue-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15a4.5 4.5 0 004.5 4.5H18a3.75 3.75 0 001.332-7.257 3 3 0 00-3.758-3.848 5.25 5.25 0 00-10.233 2.33A4.502 4.502 0 002.25 15z" />
+            </svg>
+          </div>
+          <div class="col-span-2">
+            <h3 class="text-7xl font-bold">10°</h3>
+            <p class="mt-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci necessitatibus laboriosam similique eligendi veritatis nostrum aliquid, nam voluptatum deleniti sit, unde odit eum! Praesentium iste quasi laboriosam voluptas ea perspiciatis.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bg-white shadow rounded-lg border-t-4 border-blue-800 w-full">
+      <div class="px-10 py-4 border-b border-black">
+        <h2 class="text-xl font-bold">Rio de Janeiro</h2>
+      </div>
+      <div class="px-10 py-4">
+        <div class="grid grid-cols-3 gap-4">
+          <div class="col-span-1">
+            <svg class="w-52 h-52 text-blue-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+            </svg>
+          </div>
+          <div class="col-span-2">
+            <h3 class="text-7xl font-bold">30°</h3>
+            <p class="mt-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum dolores fugit voluptates? Optio amet dolores distinctio reprehenderit. Excepturi vero itaque numquam fugiat distinctio laborum laboriosam.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
